ef4926210d11c1acd8bced82d29826dd95a13934
root <root@localhost.localdomain>
2016-10-01 20:21:16 -0400

    Second Update

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 require_once("Lyft.php");
 
+function dollarConversion($convNum, $dollarConv){
+	$cents = (int)($convNum % $dollarConv);
+       	$dollar = (int)($convNum/$dollarConv);
+        if( $cents < 10 ) {
+        	$cents = '0' . $cents;
+        }
+	echo '$' . $dollar . '.' . $cents;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -22,9 +31,43 @@ require_once("Lyft.php");
 				foreach( $ride->pricing_details as $names => $prices)
 				{
 					if($names == 'base_charge'){
-
+						echo 'Base Charge: ';
+						dollarConversion($prices, $dollarConv);
+						echo '<br>';
 					}
-					
+					elseif($names == 'cost_per_mile'){
+						echo 'Cost per Mile: ';
+						dollarConversion($prices, $dollarConv);
+						echo '<br>';
+					}
+					elseif($names == 'cost_per_minute'){
+						echo 'Cost per Minute: ';
+						dollarConversion($prices, $dollarConv);
+						echo '<br>';
+					}
+					elseif($names == 'cost_minimum')
+					{
+						echo 'Minimum Cost: ';
+						dollarConversion($prices, $dollarConv);
+						echo '<br>';
+					}
+					elseif($names == 'trust_and_service')
+					{
+						echo 'Trust and Service Fee: ';
+						dollarConversion($prices, $dollarConv);
+						echo '<br>';
+					}
+					elseif($names == 'currency')
+					{
+						echo 'Currency: '. $prices;
+						echo '<br>';
+					}
+					elseif($names == 'cancel_penalty_amount')
+					{
+						echo 'Cancal Penalty Fee: ';
+						dollarConversion($prices, $dollarConv);
+						echo '<br>';
+					}	
 				}
 			}
 		?>
