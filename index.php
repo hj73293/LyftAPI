54e85db3e05117ae71fc8668e3a09d609845c5cb
root <root@localhost.localdomain>
2016-10-01 23:46:27 -0400

    Third

--- index.php
+++ index.php
@@ -10,6 +10,51 @@ function dollarConversion($convNum, $dollarConv){
 	echo '$' . $dollar . '.' . $cents;
 }
 
+function getTotal($RideData){
+	if( isset($_GET['minutes']) ){
+		$totalPrices = array();
+		foreach($RideData->ride_types as $ride){
+			$total = 0;
+			foreach($ride->pricing_details as $names => $prices){					
+				if($names == 'base_charge'){
+                           	     $total = $total + ($prices/100);
+                                }
+                                elseif($names == 'cost_per_minute'){
+                	                $total = $total + (($prices/100) * $_GET['minutes']);
+                                }				
+			}
+			$totalPrices[$ride->ride_type] = $total;
+		}
+	return $totalPrices;
+	}
+}
+
+function printAllTotal($priceArray){
+	if( isset($_GET['minutes']) ){
+		echo '<br>';
+		foreach($priceArray as $name => $value){
+			echo 'Estimated cost of a ' . $name . ': ' . '$' . $value . '<br>';
+		}
+	}
+}
+
+function printTotal($priceArray){
+	if( isset($_GET['minutes']) ){
+		echo '<br>';
+		echo 'Estimated cost of a ';
+		echo $_GET['ride_type'] . ': $';
+		if( $_GET['ride_type'] == "Lyft Line" ){
+			echo $priceArray['lyft_line'];
+		}
+		elseif( $_GET['ride_type'] == "Lyft" ){
+                        echo $priceArray['lyft'];
+                }
+		elseif( $_GET['ride_type'] == "Lyft Plus" ){
+			echo $priceArray['lyft_plus'];
+		}
+	}
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +66,25 @@ function dollarConversion($convNum, $dollarConv){
 	<body>
 		<header>
 			Welcome to Lyft?
-		</header>
+		</headeri>
+		<?php
+			$totalArray = getTotal($RideData);
+			printTotal($totalArray, $RideData);
+		?>
+		<form action="index.php" method="GET">
+			<label for="ride_type">Select a ride type: </label>
+			<select name="ride_type">
+				<?php
+				foreach( $RideData->ride_types as $ride ){
+				?>
+					 <option value="<?php echo $ride->display_name; ?>"><?php echo $ride->display_name; ?></option>
+				<?php
+				}
+				?>
+			</select>
+			<br><label for="minutes">Estimate minute to destination: </label>
+			<input type="text" name="minutes" id="minutes" required>	
+		</form>
 		<?php
 			$dollarConv = 100;
 			foreach( $RideData->ride_types as $ride ){
